c062de37b0fa0d16b65fa0dcaffdb43a0b94a640
Ariel <arielkim429@gmail.com>
2022-01-22 09:48:04 +0900

    refactor: User, Document Entity Mapping

--- src/main/java/playground/domain/document/Document.java
+++ src/main/java/playground/domain/document/Document.java
@@ -3,28 +3,44 @@ package playground.domain.document;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import playground.domain.user.User;
+
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 public class Document {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Category category;
+
+    @Column(nullable = false)
     private String contents;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "approval_state")
     private ApprovalState approvalState;
-    private Long drafterId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "drafter_id")
+    private User drafter;
 
     @Builder
-    private Document(Long id, String title, Category category, String contents, ApprovalState approvalState, Long drafterId) {
+    private Document(Long id, String title, Category category, String contents, ApprovalState approvalState, User drafter) {
         this.id = id;
         this.title = title;
         this.category = category;
         this.contents = contents;
         this.approvalState = approvalState;
-        this.drafterId = drafterId;
+        this.drafter = drafter;
     }
 
 }
